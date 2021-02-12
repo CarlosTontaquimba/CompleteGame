9ccb49ae2e1046b6b7c9bd6e2f0dfe1ada4f5649
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-12 11:46:55 -0500

    Recolectar diamantes funcionando

---
+++ Assets/Scripts/DiamondCollected.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondCollected : MonoBehaviour
+{
+    //OnTriggerEnter2D detecta cuando algo entra en contacor con el boxcollider2d
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //Desactivar Sprite Renderer
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
