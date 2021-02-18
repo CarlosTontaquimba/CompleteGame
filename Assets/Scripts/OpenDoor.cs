379da9ca82dbb00de708e0ae23485ed1f69a5d35
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-18 17:00:19 -0500

    Entrar a niveles con clic funcionando

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OpenDoor : MonoBehaviour
+{
+    //Hacemos referencia al texto presione pantalla para ingresar
+    public Text text;
+    public string levelName;
+    public bool inDoor = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //En caso de que toquemos la puerta
+        if (collision.gameObject.CompareTag("Player")) //Si el que colisiona el el Player
+        {
+            text.gameObject.SetActive(true);//Enotnces activamos el texto
+            inDoor = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //En caso de que dejemos de tocar la puerta
+        if (collision.gameObject.CompareTag("Player")) //Si el que colisiona el el Player
+        {
+            text.gameObject.SetActive(false);//Enotnces activamos el texto
+            inDoor = false;
+        }
+    }
+    private void Update()
+    {
+        if (inDoor  && Input.GetMouseButtonDown(0)) //Si hacemos clic en la pantalla
+        {
+            SceneManager.LoadScene(levelName);
+        }   
+    }
+}
