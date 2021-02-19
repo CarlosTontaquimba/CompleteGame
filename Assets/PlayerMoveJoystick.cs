28cd08f4601fe6bbf1c16c2e98ec3b0b14704a18
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-19 11:44:24 -0500

    Joystick funcionando en su totalidad

--- Assets/PlayerMoveJoystick.cs
+++ Assets/PlayerMoveJoystick.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class PlayerMoveJoystick : MonoBehaviour
 {
     private float horizontalMove = 0f;
-    private float verticalMove = 0f;
     public Joystick joystick;
     public float runSpeedHorizontal = 2;
     public float runSpeed = 1.25f;
-    public float jumpSpeed = 3;
-    public float doubleJumpSpeed = 2.5f;
+    public float jumpSpeed = 4.5f;
     private bool canDoubleJump;
     Rigidbody2D rb2d;
     public SpriteRenderer spriteRenderer;
@@ -54,7 +52,7 @@ public class PlayerMoveJoystick : MonoBehaviour
     public void Jump()
     {
         //SALTAR
-        if (Input.GetKeyDown("up") && CheckGround.isGrounded) // En el momento que presionamos up y este en el suelo
+        if (CheckGround.isGrounded) //Cuando este en el suelo
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed); // Entonces salte
         }
