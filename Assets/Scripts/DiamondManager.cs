0fbf49f128368126d3d1100bbf649648a821686f
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-19 12:41:16 -0500

    Points counter added ^^

--- Assets/Scripts/DiamondManager.cs
+++ Assets/Scripts/DiamondManager.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 //Para cambiar de escena 
 using UnityEngine.SceneManagement;
 
 public class DiamondManager : MonoBehaviour
 {
+    public Text totalDiamonds; //Para saber todas los diamantes que hay en el nivel
+    public Text diamondsCollected; //Para saber los diamantes que emos cogido
+    private int totalDiamondsInLevel; 
     public GameObject transition;
+    private void Start()
+    {
+        totalDiamondsInLevel = transform.childCount;
+    }
     private void Update()
     {
         AllDiamondsCollected();
+        //Las diamantes que hay en todo el nivel y poder ver por pantalla
+        totalDiamonds.text = totalDiamondsInLevel.ToString();
+        //Para contar los diamantes que vamos recolectando
+        diamondsCollected.text = transform.childCount.ToString();
     }
     //Este metodo detecta si el diamante es recogido 
     public void AllDiamondsCollected()
