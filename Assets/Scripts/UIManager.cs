6b96674a76318ba2004dd4237aa2005ae36549d7
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-19 14:05:18 -0500

    Menu options added everything working

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//ESTE SCRIPT GESTIONA TODOS LOS CONTROLES PARA EL MENU DE OPCIONES 
+public class UIManager : MonoBehaviour
+{
+    //Referencia al panel de mennu
+    public GameObject optionsPanel;
+    public void OptionsPanel()
+    {
+        //Paramos el tiempo en unity 
+        Time.timeScale = 0;
+        //Activamos el panel 
+        optionsPanel.SetActive(true);
+    }
+    public void Return()
+    {
+        //Activamos el tiempo en unity
+        Time.timeScale = 1;
+        //Desactivamos el panel de options
+        optionsPanel.SetActive(false);
+    }
+    public void AnotherOptions()
+    {
+        //Sound
+        //Music
+        //Grphics
+    }
+    //Poder ir al menu principal
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    //Salir del juego 
+    public void QuitGame()
+    {
+        //Esto funcionarà unicmente cuando se saca el apk del juego
+        Application.Quit();
+    }
+}
