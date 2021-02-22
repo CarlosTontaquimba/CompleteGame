22f6a39f84c99d0f594cc0445a5b54487fc6673f
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-22 15:42:06 -0500

    Enemy sombrero added to play screen everything work

---
+++ Assets/BulletMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public GameObject startPoint, endPoint;
+    public float enemySpeed;//Velocidad de movimiento
+    public bool isGoingRigth; //Si el jugador se esta moviendohacia la derecha o izquierda
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isGoingRigth) // Si se mueve hacia la derecha
+        {
+            transform.position = startPoint.transform.position;
+        }
+        else // Si se mueve a la izquirda
+        {
+            transform.position = endPoint.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGoingRigth)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position, enemySpeed * Time.deltaTime);
+            if (transform.position == endPoint.transform.position)
+            {
+                isGoingRigth = true;
+            }
+        }
+        if (isGoingRigth)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPoint.transform.position, enemySpeed * Time.deltaTime);
+            if (transform.position == startPoint.transform.position)
+            {
+                isGoingRigth = false;
+            }
+        }
+    }
+}
---
+++ Assets/IABasic.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IABasic : MonoBehaviour
+{
+    //Este script controla los movimientos(de un lado al otr) del enemigo sombrero
+
+   
+}
