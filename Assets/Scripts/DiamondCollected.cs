b6ebd9200f7f53043a252fe25f3c3f8c2bcfba42
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-23 18:28:53 -0500

    Sounds of diamonds added

--- Assets/Scripts/DiamondCollected.cs
+++ Assets/Scripts/DiamondCollected.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class DiamondCollected : MonoBehaviour
 {
+    public AudioSource audioSource;
     //OnTriggerEnter2D detecta cuando algo entra en contacor con el boxcollider2d
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,6 +14,8 @@ public class DiamondCollected : MonoBehaviour
             GetComponent<SpriteRenderer>().enabled = false;
             //Destruimos objeto
             Destroy(gameObject, 0.5f);
+            //Activamos sonido de points
+            audioSource.Play();
 
         }   
     }
