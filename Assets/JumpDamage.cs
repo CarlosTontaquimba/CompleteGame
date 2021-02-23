75c60356494763c8dc71bf60856c2a43d0acc306
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-23 18:13:54 -0500

    Enemy kill added but not working everything

---
+++ Assets/JumpDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JumpDamage : MonoBehaviour
+{
+    //Para saber si estamos colisionando 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //Hcer que el player rebote al matar al enemigo
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,200f));
+            Destroy(transform.parent.gameObject); //Destruimos el objeto padre
+            //Destroy(gameObject); //Destruimos unicamente el objeto hijo
+        }
+    }
+
+}
