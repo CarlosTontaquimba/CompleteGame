c075f6400a8b86f156f50e058ea735395fca9560
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-26 12:00:37 -0500

    Jump on de movible platform is working

---
+++ Assets/EnemyGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject[] enemyPrefab;
+    //Rango de tiempo para generar enemigo
+    //public float  minTime = 1.5f,maxTime = 4f;
+    //***
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateEnemy();
+    }
+    void CreateEnemy()
+    { 
+        Instantiate(enemyPrefab[Random.Range(0, enemyPrefab.Length)], transform.position, Quaternion.identity);
+    }
+    
+}
---
+++ Assets/PlatformMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            //El momento que colisionamos 
+            collision.collider.transform.SetParent(transform);
+            CheckGround.isGrounded = true;
+            Debug.Log(CheckGround.isGrounded);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            collision.collider.transform.SetParent(null);
+            CheckGround.isGrounded = false;
+            Debug.Log(CheckGround.isGrounded);
+        }
+    }
+}
