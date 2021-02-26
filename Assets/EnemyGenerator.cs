e8ad6338e4368fb7022e963a1cb94625d524e3cc
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-26 12:58:34 -0500

    Enemi generator working

--- Assets/EnemyGenerator.cs
+++ Assets/EnemyGenerator.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
-    public GameObject[] enemyPrefab;
+    //public GameObject[] enemyPrefab;
+    public GameObject enemyPrefab;
     //Rango de tiempo para generar enemigo
-    //public float  minTime = 1.5f,maxTime = 4f;
+    public float gereatorTimer = 1.75f;
     //***
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //InvokeRepeating("CreateEnemy",0f, gereatorTimer); //El 0f es el tiempo de retardo que tiene la primera vez que se ejecuta esta invocacion y despues cada 1.75 segundos
     }
 
     // Update is called once per frame
     void Update()
     {
-        CreateEnemy();
+
     }
     void CreateEnemy()
-    { 
-        Instantiate(enemyPrefab[Random.Range(0, enemyPrefab.Length)], transform.position, Quaternion.identity);
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
     }
-    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("CreateEnemyActivator"))
+        {
+            Debug.Log("Generador golpeo a activador");
+            InvokeRepeating("CreateEnemy", 0f, gereatorTimer); //El 0f es el tiempo de retardo que tiene la primera vez que se ejecuta esta invocacion y despues cada 1.75 segundos
+        }
+    }
+
 }
