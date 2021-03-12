66c91356f9d3a5d0144cd20c73b55ed227923d6f
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-03-12 14:02:31 -0500

    Canvas open whit collision and close when button in pressed , everything is working ok

---
+++ Assets/popUP.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class popUP : MonoBehaviour
+{
+    public Canvas canvas;
+    public bool a = false;
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            PopUp();
+        }
+    }
+    public void PopUp()
+    {
+        if (a == false)
+        {
+            a = true;
+            canvas.enabled = true;
+        }
+    }
+    public void Return()
+    {
+        if (a == true)
+        {
+            a = false;
+            canvas.enabled = false;
+        }
+    }
+
+
+}
