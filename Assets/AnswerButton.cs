34978a7302f6a2b81d23761a20d4415924719450
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-03-16 13:06:47 -0500

    Working in quiz panel

---
+++ Assets/AnswerButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+    public Text answerText;
+    private AnswerData answerData;
+    public void Setup(AnswerData data)
+    {
+        answerData = data;
+        answerText.text = answerData.answerText;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
