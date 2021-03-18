a23c7dd7fc8d286b9a28ec5efa8e2626dd05d047
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-03-18 18:29:32 -0500

    Dialog System is working

---
+++ Assets/Scripts/Transition Effect for Dialog System/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] //Esto le da visibilidad a ls variables dentro del inspector
+public class Dialogue
+{
+    //ESTA CLASE DETERMINA EL NOMBRE DEL PERSONAJE Y EL DICTADO QUE VA HA TENER 
+
+    public string name; //Nombre del personaje
+    [TextArea(3,10)]//Esto determina el valor minimo de lineas y el valor maximo de lineas  [TextArea] => Sin limite
+    public string[] sentenceList;//Listado de oraciones
+}
---
+++ Assets/Scripts/Transition Effect for Dialog System/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    //MANEGA LAS ORACIONES DE CADA UNO DE LOS PERSONAJES
+
+    public Dialogue dialogue;
+    Queue<string> sentences; //Es un listado de objetos en este caso de oraciones
+    public GameObject dialoguePanel; //Aqui esta el panel que contiene el texto
+    public Text displayText; //Texto que representa cada uno d elas oraciones
+    string activeSentence; //COntiene el valor de la oracion actual
+    public float typingSpeen; //Rapidez con la que la oracion se irá creando
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    void StartDialogue()
+    {
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentenceList)
+        {
+            sentences.Enqueue(sentence); // LO que hace el Enqueue es toma la oracion y la añade al queue para ser presentado 
+        }
+        DisplayNextSentence();
+    }
+    void DisplayNextSentence()
+    {
+        if (sentences.Count <= 0)
+        {
+            displayText.text = activeSentence;
+            return;
+        }
+        activeSentence = sentences.Dequeue();//Dequeue saca la oracion de el listado y la pasa a la variable active sentences
+        displayText.text = activeSentence;
+        //Debug.Log(activeSentence);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            StartDialogue();
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                DisplayNextSentence();
+            }
+        }
+    }
+}
