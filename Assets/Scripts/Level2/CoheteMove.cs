d16bcab782e71e7096abcc23bc0a4a5bf788a71f
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-03-24 17:58:02 -0500

    Enemy cohete is working ok

---
+++ Assets/Scripts/Level2/CoheteMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoheteMove : MonoBehaviour
+{
+    // ESTE SCRIPT HACE QUE EL COHETE SE MUEVA DE DERECHA A IZQUIERDA
+    public float velocity = 2f;
+    private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = Vector2.left * velocity; // Hace que un objeto se mueca hacia la izquierda
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Level2/EnemyGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float generatorTimer = 7f; //Cada cuanto se genera un enemigo
+
+    private void Start()
+    {
+        InvokeRepeating("CreateCohete", 0f, generatorTimer);
+    }
+    void CreateCohete()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+}
