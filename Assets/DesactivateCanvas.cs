48c701979acac637528c210c5650abd27a4f1ae8
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-04-14 14:04:08 -0500

    Not added

---
+++ Assets/DesactivateCanvas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesactivateCanvas : MonoBehaviour
+{
+    public Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            canvas.enabled = true;
+        }
+    }
+}
